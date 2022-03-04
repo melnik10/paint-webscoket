ea90cb6ad0681a1b3583c9b70d619fb29723a1e8
Vladislav <vladislavxperiaz3@gmail.com>
2022-03-04 14:49:38 +0200

    1. Paint-websocket.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {FC} from 'react';
+import './styles/app.scss'
+import ToolBar from "./components/Toolbar";
+import SettingBar from "./components/Settingbar";
+import Canvas from "./components/Canvas";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App: FC = () => {
+    return (
+        <div className={'app'}>
+            <ToolBar/>
+            <SettingBar/>
+            <Canvas/>
+        </div>
+    );
 }
 
 export default App;
